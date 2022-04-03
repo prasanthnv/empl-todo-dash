b6fc40a32033811a51dcdac55f6ca51a999e8414
Prasanth Venugopal <prasanthnvenugopal@gmail.com>
2022-04-04 03:55:01 +0530

    Completed app module and config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './shared/guard/auth/auth.guard';
+import { LoggedGuard } from './shared/guard/logged/logged.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate:[LoggedGuard]
+  },
+  {
+    path: 'employee',
+    loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule),
+    canActivate: [AuthGuard]
+  },
+  {path: '', redirectTo: 'auth', pathMatch: 'full'},
+  { path: '**', redirectTo: 'auth', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { LoaderService } from './shared/services/loader/loader.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'empl-todo-dash';
+  loaderRef: any;
+  constructor(private loaderService:LoaderService, private dialog: MatDialog){}
+
+  ngOnInit(): void {
+    this.loaderService.getLoader().subscribe(count=>{
+        console.log('show loader', count);
+        if(count){
+          this.loaderRef = this.dialog.open(LoaderComponent);
+        }else{
+          this.loaderRef?.close();
+        }
+    })
+  }
 }
